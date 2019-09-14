6c01a668fb6fa433230acfc8960a8b7387fb9190
Haithem Benchaaben <haythem.benchaben@enis.tn>
2019-09-14 11:35:08 +0100

    add new module eureka metadata arrangement

---
+++ eureka-clients-metadata-arrangement/src/main/java/com/dashboard/EurekaClientsMetadataArrangementApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientsMetadataArrangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientsMetadataArrangementApplication.class, args);
+    }
+
+}
